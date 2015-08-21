b388d403e16f65789b1893ea71812be936ccd65c
Jordan Verasamy <j.verasamy@gmail.com>
2015-08-20 20:57:20 -0400

    add round logic

--- Challonge/downloader.py
+++ Challonge/downloader.py
@@ -20,19 +20,31 @@ for participant in challonge.participants.index(tournament["id"]):
 	participant_ids[participant["id"]] = participant["display-name"]
 	participant_names.append(participant["display-name"])
 
+num_participants = len(participant_names)
+
 for match in match_list:
-	condensed_match = {"winner" : participant_ids[match["winner-id"]],
-	                    "loser" : participant_ids[match["loser-id"]]}
-	condensed_matches.append(condensed_match)
+	if match["state"] == "complete":
+		condensed_match = {"winner" : participant_ids[match["winner-id"]],
+		                    "loser" : participant_ids[match["loser-id"]],
+		                    "round" : match["round"]}
+		condensed_matches.append(condensed_match)
 
 for name in participant_names:
-	participant_records[name] = {"wins" : 0, "losses" : 0}
+	participant_records[name] = {"wins" : 0, "losses" : 0, "final_round" : None, worst_possible_round : None}
 
 for condensed_match in condensed_matches:
-	participant_records[condensed_match["winner"]]["wins"] += 1
-	participant_records[condensed_match["loser"]]["losses"] += 1
+	winner = condensed_match["winner"]
+	loser = condensed_match["loser"]
+
+	participant_records[winner]["wins"] += 1
+	participant_records[loser]["losses"] += 1
+
+	#need some way to compute the worst possible round...
+
+	if participant_records[loser]["losses"] == 2:
+		participant_records[loser]["final_round"] = condensed_match["round"]
 
 pprint.pprint(participant_records)
-	
+print num_participants
 
 
