cb1163f35a801448e93624df313b0620c59b2b70
Jordan Verasamy <j.verasamy@gmail.com>
2015-08-24 18:02:39 -0400

    this probably won't work, need to restructure

--- Challonge/downloader.py
+++ Challonge/downloader.py
@@ -5,16 +5,16 @@ import json
 
 class Tournament:
 
-	participant_data = {}
-	participant_ids = {}
-	participant_names = []
-	condensed_matches = []
-
-	username = ''
-	tournament_url = ''
-	api_key = ''
-
 	def __init__(self, username, tournament_url, api_key):
+
+		self.username = username
+		self.tournament_url = tournament_url
+		self.api_key = api_key
+
+		self.participant_data = {}
+		self.participant_ids = {}
+		self.participant_names = []
+		self.condensed_matches = []
 
 		self.username = username
 		self.tournament_url = tournament_url
@@ -28,22 +28,30 @@ class Tournament:
 		match_list = challonge.matches.index(tournament['id'])
 
 		for participant in challonge.participants.index(tournament['id']):
-			self.participant_ids[participant['id']] = participant['display-name']
-			self.participant_names.append(participant['display-name'])
+			name = participant['display-name']
+			self.participant_ids[participant['id']] = name
+			if name not in participant_names:
+				self.participant_names.append(name)
 
 		num_participants = len(self.participant_names)
+
+		updated_condensed_matches = []
 
 		for match in match_list:
 			if match['state'] == 'complete':
 				condensed_match = {'winner' : self.participant_ids[match['winner-id']],
 				                    'loser' : self.participant_ids[match['loser-id']],
 				                    'round' : match['round']}
-				self.condensed_matches.append(condensed_match)
+				updated_condensed_matches.append(condensed_match)
+
+		num_new_matches = len(updated_condensed_matches) - len(self.condensed_matches)
+
+		new_condensed_matches = updated_condensed_matches[num_new_matches:]
 
 		for name in self.participant_names:
 			self.participant_data[name] = {'wins' : 0, 'losses' : 0, 'final_round' : None, 'worst_possible_round' : None}
 
-		for condensed_match in self.condensed_matches:
+		for condensed_match in new_condensed_matches:
 			winner = condensed_match['winner']
 			loser = condensed_match['loser']
 
