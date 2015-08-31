603aa73c6090d70bd9d53e410227a7c1109b5da2
Jordan Verasamy <j.verasamy@gmail.com>
2015-08-30 20:25:41 -0400

    encapsulate response behaviour in functions

--- Challonge/slackbot.py
+++ Challonge/slackbot.py
@@ -16,22 +16,26 @@ channel_id = config.channels[channel_name]
 tournament_tracker = TournamentTracker(username, tournament_url, challonge_api_key)
 slack_client = SlackClient(slack_api_token)
 
+def execute_command(command):
+	slack_client.rtm_send_message(channel_id, 'executing command {}'.format(command))
+
+def create_update_message(message_list):
+	update_message = '```'
+	for condensed_match in new_matches:
+		update_message = update_message + str(condensed_match) + '\n'
+	return update_message + '```'
+
 if slack_client.rtm_connect():
 	while True:
 		new_messages = slack_client.rtm_read()
 		new_matches = tournament_tracker.pull_matches()
 
 		for message in new_messages:
-			print message
-			if 'text' in message:
-				if message['text'] == 'pls 20xx me' and message['channel'] == channel_id:
-					slack_client.rtm_send_message(channel_id, 'you have been 20xx\'d')
+			if 'text' in message and message['channel'] == channel_id and message['text'][:5] == '!20xx':
+				execute_command(message['text'][5:])
 
 		if new_matches:
-			update_message = '```'
-			for condensed_match in new_matches:
-				update_message = update_message + str(condensed_match) + '\n'
-			update_message = update_message + '```'
+			update_message = create_update_message(new_matches)
 			slack_client.rtm_send_message(channel_id, update_message)
 			print 'Sent updates to #{}!'.format(channel_name)
 		else:
