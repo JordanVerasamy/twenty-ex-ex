59eb9ed75e8275a85750d2c6378d775ca35c3b21
Jordan Verasamy <j.verasamy@gmail.com>
2016-01-07 02:31:00 -0500

    fix up config template

--- Challonge/config_TEMPLATE.py
+++ Challonge/config_TEMPLATE.py
@@ -1,5 +1,4 @@
 challonge_username = 'YOUR_NAME_HERE'
-tournament_url = 'YOUR_URL_HERE'
 
 challonge_api_key = 'YOUR_KEY_HERE'
 slack_api_token = 'YOUR_TOKEN_HERE'
@@ -11,15 +10,9 @@ channel_to_connect_to = 'NAME'
 
 # The Challonge username is just your Challonge account name.
 
-# The Challonge tournament URL should contain the subdomain first, if it exists, 
-# then the tournament's URL tag. For example, if your tournament is at 
-# `contrivedcup.challonge.com/arbitrarytournament` then 
-# you would set tournament_url equal to 'contrivedcup-arbitrarytournament', and 
-# `challonge.com/mytournament` would be 'mytournament'.
-
 # You can get your Challonge API key from: https://challonge.com/settings/developer
 
-# You can get your Slack API token by adding a Bots integration to your 
+# You can get your Slack API token by adding a Bots integration to your
 # Slack channel and clicking `Configure`.
 
 # Slack channel IDs can be retrieved by querying the server for messages and checking
